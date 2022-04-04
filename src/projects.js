5e07df2730360bf5c0b9fea034c60f76080fe669
Andrew Glach <andrew.glach@gmail.com>
2022-04-04 06:46:14 -0600

    Changed due date to calendar input, attempted to add clear complete, not ready yet

--- src/projects.js
+++ src/projects.js
@@ -16,9 +16,19 @@ export function projects() {
     let projectLibrary = [];
 
     // Create New Project
+    const inputLabel = document.createElement('label');
+    inputLabel.setAttribute('for', 'project-name');
+    inputLabel.innerHTML = 'Project Name';
+
     const inputArea = document.createElement('input');
     inputArea.setAttribute('id', 'project-name');
-    libraryDiv.appendChild(inputArea);
+
+    const inputDiv = document.createElement('div');
+    inputDiv.classList.add('input-div');
+    inputDiv.appendChild(inputLabel);
+    inputDiv.appendChild(inputArea);
+
+    libraryDiv.appendChild(inputDiv);
 
     const createProject = document.createElement('button');
     createProject.innerHTML = 'Create New Project';
@@ -37,6 +47,7 @@ export function projects() {
         const title = document.getElementById('project-name').value;
         const newProject = projectFactory(title);
         projectLibrary.push(newProject)
+        inputArea.value = null;
     }    
 
     function publishLibrary() {
@@ -47,8 +58,8 @@ export function projects() {
             projectName.setAttribute('id', i)
             projectName.innerHTML = projectLibrary[i].title;
 
-            projectName.addEventListener('click', () => {
-                let number = event.target.id
+            projectName.addEventListener('click', (e) => {
+                let number = e.target.id
 
                 mainDiv.innerHTML = null;
 
@@ -66,11 +77,24 @@ export function projects() {
                 const toDoName = document.createElement('input');
                 toDoName.setAttribute('id', 'to-do-name');
 
+                const toDoNameLabel = document.createElement('label');
+                toDoNameLabel.setAttribute('for', 'to-do-name');
+                toDoNameLabel.innerHTML = 'To Do Name';
+
                 const dueDate = document.createElement('input');
+                dueDate.setAttribute('type', 'date');
                 dueDate.setAttribute('id', 'due-date');
+
+                const dueDateLabel = document.createElement('label');
+                dueDateLabel.setAttribute('for', 'due-date');
+                dueDateLabel.innerHTML = 'Due Date';
+
+                const todoInputDiv = document.createElement('div');
+                todoInputDiv.classList.add('to-do-input-div');
             
                 const createToDo = document.createElement('button');
                 createToDo.innerHTML = 'Create New To Do';
+                createToDo.setAttribute('id', 'create-to-do');
                 createToDo.addEventListener('click', () => {
                     const title = (document.getElementById('to-do-name').value);
                     const description = (document.getElementById('due-date').value);
@@ -81,12 +105,31 @@ export function projects() {
                     arrayDiv.appendChild(projectArrayP);
                     console.log(projectArray);
                     mainDiv.appendChild(arrayDiv);
+                    toDoName.value = null;
+                    dueDate.value = null;
                 })
 
+                // This functionality is still in process.
+                // const clearCompleted = document.createElement('button');
+                // clearCompleted.innerHTML = 'Clear Completed Tasks';
+                // clearCompleted.addEventListener('click', () => {
+                //     let list = document.querySelectorAll('.checkbox');
+                //     for (i = 0; i < list.length; i++) {
+                //         if ()
+                //     }
+                // })
+
+                todoInputDiv.appendChild(toDoNameLabel);
+                todoInputDiv.appendChild(dueDateLabel);
+                todoInputDiv.appendChild(toDoName);
+                todoInputDiv.appendChild(dueDate);
+                todoInputDiv.appendChild(createToDo);
+                
+
                 mainDiv.appendChild(title);
-                mainDiv.appendChild(toDoName);
-                mainDiv.appendChild(dueDate);
-                mainDiv.appendChild(createToDo);
+                mainDiv.appendChild(todoInputDiv);
+                // mainDiv.appendChild(toDoName);
+                // mainDiv.appendChild(dueDate);
                 mainDiv.appendChild(arrayDiv);
                 publishProjectArray();
                 
@@ -99,12 +142,19 @@ export function projects() {
                         const toDoDescription = document.createElement('p');
                         toDoTitle.innerHTML = projectArray[i].title;
                         toDoDescription.innerHTML = projectArray[i].description;
+
+                        // const completedBox = document.createElement('input');
+                        // completedBox.setAttribute('id', i);
+                        // completedBox.classList.add('checkbox');
+                        // completedBox.setAttribute('type', 'checkbox')
+                        // completedBox.innerHTML = 'Completed';
+
                         const deleteButton = document.createElement('button');
                         deleteButton.setAttribute('id', i);
-                        deleteButton.innerHTML = 'Delete';
+                        deleteButton.innerHTML = 'Completed';
             
-                        deleteButton.addEventListener('click', () => {
-                            const number = event.target.id;
+                        deleteButton.addEventListener('click', (e) => {
+                            const number = e.target.id;
                             if (number > -1) {
                                 projectArray.splice(number, 1);
                             }
@@ -113,6 +163,7 @@ export function projects() {
             
                         toDoDiv.appendChild(toDoTitle);
                         toDoDiv.appendChild(toDoDescription);
+                        // toDoDiv.appendChild(completedBox);
                         toDoDiv.appendChild(deleteButton);
             
                         projectArrayP.appendChild(toDoDiv);
